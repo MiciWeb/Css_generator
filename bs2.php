93b0b819ade6631a997b41fc0f9badddb8c45d86
Micipsa <micipsa.sersour@epitech.eu git config --global user.name mici92exitcd>
2021-06-01 14:19:39 +0200

    bootstrap v2 'finit nrmlment'

--- bs2.php
+++ bs2.php
@@ -1,17 +1,15 @@
 <?php
 function my_scandir($dir_path){
     $dir = opendir($dir_path);
-
+    $array = [];
         while(($file = readdir($dir)) !== false) 
     {  
-    if (preg_match("/.png/i","$file") == 1){
-        $array = [];
+    if (substr($file,-3) == "png"){
         array_push($array,$file);
-        foreach($array as $value){
-            print_r($array);
-        }
+       
     }
     }
+    print_r($array);
     
     closedir($dir);
 }
