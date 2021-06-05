ed332be8565c4969d6633367a6a67cddbdbead76
Micipsa <micipsa.sersour@epitech.eu git config --global user.name mici92exitcd>
2021-06-05 13:06:32 +0200

    reste à organiser et a faire les bonus

--- rcv.php
+++ rcv.php
@@ -1,16 +1,34 @@
 <?php
 function recursive($argv){
     array_shift($argv);
-    if(implode(" ",$argv) == "."){ 
+    if(is_dir(implode(" ",$argv))){ 
         $dir = opendir(implode(" ",$argv));
         $array = [];
         while(($files = readdir($dir)) !== false) {  
             if (substr($files,-3) == "png"){
             array_push($array,$files);
-        }
-        var_dump($array);
+            }
+            recursive($argv);
+
         }
         closedir($dir);
+
+        var_dump($array);
     }
 }
-recursive($argv);
+function scan($dir){
+    if ($dh = opendir($dir)){
+        while (($file = readdir($dh)) !== false){
+            if ($file !== "." AND $file !== ".." AND $file !== ".git"){
+                if (is_dir($dir . $file)){
+                    scan($dir . $file ."/");
+
+                } elseif(pathinfo($file, PATHINFO_EXTENSION) == "png"){
+                    echo $file . "\n";
+                }
+        }
+    }
+    closedir($dh);
+    }
+}
+scan(".");
--- test.php
+++ test.php
@@ -1,41 +1,40 @@
 <?php
-function spriter($files) {
-    //function Generate Sprite
-  $width = 0;
-  $imgwidth = 30;
-  $imgheight = 30;
-  foreach ($files as $file) {
-      $width += $imgwidth;
-  }
+// function spriter($files) {
+//     //function Generate Sprite
+//   $width = 0;
+//   $imgwidth = 30;
+//   $imgheight = 30;
+//   foreach ($files as $file) {
+//       $width += $imgwidth;
+//   }
 
-  $dest = imagecreatetruecolor($width, $imgheight);
+//   $dest = imagecreatetruecolor($width, $imgheight);
 
-  $pos = 0;
-  foreach ($files as $file){
-    $source = imagecreatefrompng($file);
+//   $pos = 0;
+//   foreach ($files as $file){
+//     $source = imagecreatefrompng($file);
     
-    imagecopy($dest, $source, $pos, 0, 0, 0, $imgwidth, $imgheight);
+//     imagecopy($dest, $source, $pos, 0, 0, 0, $imgwidth, $imgheight);
 
-    $pos += $imgwidth;
-    }
-      imagepng($dest,"sprite.png");
+//     $pos += $imgwidth;
+//     }
+//       imagepng($dest,"sprite.png");
 
     //function Generate Css
       $fichier = fopen('style.css', "c");
-    fwrite($fichier, "html{\nposition: relative;\nheight: 30px;\nwidth: 30px;\n}\n.image{\nbackground: url('sprite.png') no-repeat;\nwidth:100vw;\nheight: 30px;\nleft: 0;\ntop: 0;\n}\n");
-    fclose($fichier);
-     foreach ($files as $key => $file){
-        $key1 = $key+1;
-        $clip -= 30;
-        $fichier = 'style.css';
-        $content = ".image-".$key1."{\nposition: absolute;\nbackground: url('sprite.png') no-repeat;\nwidth:100%;\nheight: 30px;\nleft: 0;\ntop: 0;\nbackground-position: ".$clip."px;\n}\n";
-        file_put_contents($fichier,$content,FILE_APPEND);
-    }
+//     fwrite($fichier, "html{\nposition: relative;\nheight: 30px;\nwidth: 30px;\n}\n.image{\nbackground: url('sprite.png') no-repeat;\nwidth:100vw;\nheight: 30px;\nleft: 0;\ntop: 0;\n}\n");
+//     fclose($fichier);
+//      foreach ($files as $key => $file){
+//         $key1 = $key+1;
+//         $clip -= 30;
+//         $fichier = 'style.css';
+//         $content = ".image-".$key1."{\nposition: absolute;\nbackground: url('sprite.png') no-repeat;\nwidth:100%;\nheight: 30px;\nleft: 0;\ntop: 0;\nbackground-position: ".$clip."px;\n}\n";
+//         file_put_contents($fichier,$content,FILE_APPEND);
+//     }
     
-  }
-spriter(glob("*.png"));
-?>
-<?php
+//   }
+// spriter(glob("*.png"));
+
 // # Les sprites permettent de réduire le temps de chargement d'une page en concatenant plusieurs image en une.
 // # + optimisation car - requete, pas besoin de onmouseover js
 // // $first = imagecreatefrompng("img/playstation.png");
@@ -73,3 +72,6 @@ spriter(glob("*.png"));
 // 	}
 // }
 // mytest($argc,$argv);
+$secondlast = array_slice($argv,-2,1);
+
+var_dump(implode(" ",$secondlast));
