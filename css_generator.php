f24c3435650e1138b59b2a5c5fd7ca27acac1879
Micipsa <micipsa.sersour@epitech.eu git config --global user.name mici92exitcd>
2021-07-06 16:40:41 +0200

    css generator

--- css_generator.php
+++ css_generator.php
@@ -23,7 +23,7 @@ function my_scan_dir($argv){
                     unset($argv[$array_if_option_exist +1]);
                     unset($argv[$array_if_option_exist]);
                 }
-        
+
     // NOTE option -s --output-style
         if (in_array("-s",$argv) OR in_array("--output-style",$argv) ){
              if(in_array("-s",$argv)){
@@ -51,7 +51,7 @@ function my_scan_dir($argv){
                 }
                 $array_without_option = end($argv);
                 unset($argv[$array_if_option_exist]);
-                
+
                 // transforme argv en string pour que la recursive marche
                 $dir_path = [];
                 array_push($dir_path,$array_without_option);
@@ -67,7 +67,7 @@ function my_scan_dir($argv){
             $array = [];
                 while(($file = readdir($dh)) !== false){
                     if (substr($file,-3) == "png"){
-                        array_push($array,$file);
+                        array_push($array,end($argv)."/".$file);
                     }
                 }
             closedir($dh);
@@ -80,22 +80,9 @@ function my_scan_dir($argv){
                     array_push($array,$files);
                 }
             }
-            
-    // // NOTE option -c --columns_number (bonus)
-    // if (in_array("-c",$argv) OR in_array("--columns_number",$argv) ){
-    //     if(in_array("-i",$argv)){
-    //         $array_if_option_exist = array_search("-c",$argv);
-    //     }else{
-    //         $array_if_option_exist = array_search("--columns_number",$argv);
-    //     }
-    //         // sélectionne l'option et le nom de l'image
-    //         $column_number = $argv[$array_if_option_exist +1];
-    //         array_splice($array,$column_number);
-    //         // supprime l'option et le nom de l'image
-    //         unset($argv[$array_if_option_exist +1]);
-    //         unset($argv[$array_if_option_exist]);
-    //     }
-    
+            var_dump(end($argv));
+            var_dump($array);
+
     generate_sprite($array);
 }
 my_scan_dir($argv);
@@ -107,9 +94,9 @@ function my_recursive($dir_path_string){
              if ($file !== "." AND $file !== ".." AND $file !== ".git"){
                  if (is_dir($dir_path_string.$file)){
                      my_recursive($dir_path_string.$file."/");
-                     
+
                  } elseif(substr($file,-3) == "png"){
-                     array_push($array,$dir_path_string.$file); 
+                     array_push($array,$dir_path_string.$file);
                  }
          }
      }
@@ -131,20 +118,20 @@ function generate_sprite($array){
             foreach($array as $picture){
                 $source = imagecreatefrompng($picture);
                 $img_width = imagesx($source);
-                $img_height = imagesy($source); 
+                $img_height = imagesy($source);
                 array_push($img_height_max,$img_height);
                 array_push($img_width_max,$img_width);
                 array_push($all_img_width,$img_width);
-            }    
+            }
         // incrémente et corréle
             $sprite_width = 0;
             foreach ($array as $key => $picture) {
-                $sprite_width += $all_img_width[$key];   
+                $sprite_width += $all_img_width[$key];
             }
                 $destination = imagecreatetruecolor($sprite_width, max($img_height_max));
 
-            foreach($array as $key => $picture){   
-                $source = imagecreatefrompng($picture);         
+            foreach($array as $key => $picture){
+                $source = imagecreatefrompng($picture);
                 imagecopy($destination, $source, $pos_x, 0, 0, 0, $sprite_width, max($img_height_max));
                 $pos_x += $all_img_width[$key];
             }
@@ -153,7 +140,7 @@ function generate_sprite($array){
             imagepng($destination,$sprite_name);
 
     // NOTE: Génére le fichier css //
-        
+
         $fichier = fopen($style_name, "c");
         fwrite($fichier, "html{\n\tposition: relative;\n}.image{\n\tbackground: url('".$sprite_name."') no-repeat;\n\twidth:200vw;\n\theight: ".max($img_height_max)."px;\n\tleft: 0;\n\ttop: 0;\n}\n");
         // crée 2 array différent des img width
@@ -161,7 +148,7 @@ function generate_sprite($array){
             $arr = [];
             $all_img_width_for_position_x = array_merge($arr,$all_img_width);
             array_unshift($all_img_width,0);
-            
+
             foreach ($array as $key => $file){
                 $key1 = $key +1;
                 $position_x -= $all_img_width[$key];
@@ -170,5 +157,3 @@ function generate_sprite($array){
 
             fclose($fichier);
 }
-
-
